efd4c94ef80e69e13a0606963f8b1ba313030c55
bedon001 <beauclair.dongmongnintedem001@stud.fh-dortmund.de>
2021-04-09 20:58:10 +0200

    Refix as last commit

--- mamphi-gui-v2/Mamphi.sql
+++ mamphi-gui-v2/Mamphi.sql
@@ -30,46 +30,46 @@ create table Informed_consent
 (	
 	Patient_Id smallint,
     Zentrum smallint,
-    Einwilligung_erteilt varchar(30),
+    Einwilligung varchar(30),
     Datum varchar(55)
 );
 
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1000,102,'nein','10.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1001,201,'ja','11.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1002,202,'ja','12.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1003,101,'ja','13.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1004,102,'ja','13.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1005,104,'ja','13.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1006,103,'ja','13.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1007,105,'ja','00.01.1900');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1008,106,'ja','13.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1009,108,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1010,203,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1011,204,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1012,205,'ja','14.06.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1013,109,"",'14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1014,110,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1016,201,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1017,201,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1018,203,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1019,104,'ja','14.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1020,104,'ja','14.01.1900');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1021,103,'ja','15.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1022,110,'ja','16.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1023,108,'ja','16.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1024,101,'ja','16.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1025,102,'ja','15.05.2091');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1026,105,'ja','16.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1027,106,"",'00.01.1900');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1028,106,'ja','16.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1029,204,'ja','16.05.2019');
-insert into Informed_consent(Patient_Id,Zentrum,Einwilligung_erteilt,Datum_der_Einwilligung)values(1030,207,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1000,102,'nein','10.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1001,201,'ja','11.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1002,202,'ja','12.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1003,101,'ja','13.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1004,102,'ja','13.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1005,104,'ja','13.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1006,103,'ja','13.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1007,105,'ja','00.01.1900');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1008,106,'ja','13.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1009,108,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1010,203,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1011,204,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1012,205,'ja','14.06.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1013,109,"",'14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1014,110,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1016,201,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1017,201,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1018,203,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1019,104,'ja','14.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1020,104,'ja','14.01.1900');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1021,103,'ja','15.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1022,110,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1023,108,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1024,101,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1025,102,'ja','15.05.2091');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1026,105,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1027,106,"",'00.01.1900');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1028,106,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1029,204,'ja','16.05.2019');
+insert into Informed_consent(Patient_Id,Zentrum,Einwilligung,Datum)values(1030,207,'ja','16.05.2019');
 
 /*
 select * from Informed_consent;
-select * from Informed_consent where Einwilligung_erteilt = "ja";
-select * from Informed_consent where Einwilligung_erteilt = "nein";
-select * from Informed_consent where Einwilligung_erteilt = "";
+select * from Informed_consent where Einwilligung = "ja";
+select * from Informed_consent where Einwilligung = "nein";
+select * from Informed_consent where Einwilligung = "";
 
 select Zentrum_Id, Land, Ort, Prüfer, Monitor from Zentren;
 select * from Zentren where Land = "D";
@@ -83,36 +83,36 @@ create table Random_Woche_1
     Datum varchar(55)
 );
 
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1001,201,'A','12.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1002,202,'B','12.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1003,101,'A','12.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1004,102,'A','13.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1005,104,'B','13.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1006,103,'B','13.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1007,105,'A','13.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1008,106,'B','13.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1009,108,'B','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1010,203,'A','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1011,204,'A','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1012,205,'A','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1013,109,'B','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1014,110,'B','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1015,201,'A','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1016,201,'B','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1017,201,'B','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1018,203,'B','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1019,104,'A','14.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1020,104,'A','14.01.1900');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1021,103,'B','15.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1022,110,'A','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1023,108,'B','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1024,101,'A','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1025,102,'A','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1026,105,'B','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1027,106,'B','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1028,106,'A','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1029,204,'B','16.05.2019');
-insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1030,207,'A','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1001,201,'A','12.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1002,202,'B','12.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1003,101,'A','12.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1004,102,'A','13.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1005,104,'B','13.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1006,103,'B','13.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1007,105,'A','13.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1008,106,'B','13.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1009,108,'B','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1010,203,'A','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1011,204,'A','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1012,205,'A','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1013,109,'B','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1014,110,'B','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1015,201,'A','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1016,201,'B','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1017,201,'B','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1018,203,'B','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1019,104,'A','14.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1020,104,'A','14.01.1900');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1021,103,'B','15.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1022,110,'A','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1023,108,'B','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1024,101,'A','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1025,102,'A','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1026,105,'B','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1027,106,'B','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1028,106,'A','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1029,204,'B','16.05.2019');
+insert into Random_Woche_1(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1030,207,'A','16.05.2019');
 
 -- select * from Random_Woche_1
 -- select * from Random_Woche_1 where Behandlungsarm = "A";
@@ -125,30 +125,30 @@ create table Random_Woche_2
     Datum varchar(55)
 );
 
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1031,101,'B','02.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1032,102,'B','02.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1033,103,'A','02.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1034,106,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1035,108,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1036,202,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1037,202,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1038,101,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1039,203,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1040,201,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1041,104,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1042,-999,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1043,108,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1044,101,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1045,104,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1046,201,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1047,203,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1048,110,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1049,108,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1050,201,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1051,110,'B','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1052,201,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1053,101,'A','03.06.2019');
-insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum_Randomisierung)values(1054,201,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1031,101,'B','02.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1032,102,'B','02.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1033,103,'A','02.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1034,106,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1035,108,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1036,202,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1037,202,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1038,101,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1039,203,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1040,201,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1041,104,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1042,-999,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1043,108,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1044,101,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1045,104,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1046,201,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1047,203,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1048,110,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1049,108,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1050,201,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1051,110,'B','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1052,201,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1053,101,'A','03.06.2019');
+insert into Random_Woche_2(Patient_Id,Zentrum,Behandlungsarm,Datum)values(1054,201,'B','03.06.2019');
 
 -- select * from Random_Woche_2 where Behandlungsarm = 'A';
 /*
